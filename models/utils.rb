d98cfbab5eece414768b06aeb32f3b102c0c3074
Scott Chamberlain <myrmecocystus@gmail.com>
2016-02-23 17:21:05 -0800

    remove files

--- models/utils.rb
+++
@@ -1,5 +0,0 @@
-def get_count(table, string)
-  query = sprintf("SELECT count(*) as ct FROM %s %s", table, string)
-  res = $client.query(query, :as => :json)
-  res.collect{ |row| row }[0]["ct"]
-end
